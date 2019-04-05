2ae6d372d1f7239f5809eaf74bfda74ad6d23a1a
Vladimir Konkov <vkonkov@citc.ru>
2019-04-05 16:20:12 +0400

    For version 1.3.21

---
+++ README.md
@@ -0,0 +1,9 @@
+# kotlin-jdk8-osgi-bundle
+Kotlin Standart Libraries repackaged as OSGI super-bundle.
+
+Bundle includes:
+ - stdlib
+ - stdlib-jdk7
+ - stdlib-jdk8
+ - reflect
+ 
---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    java
+    id("biz.aQute.bnd.builder") version "4.1.0"
+    `maven-publish`
+}
+
+group = "org.jetbrains.kotlin"
+version = "1.3.21"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib-jdk8", project.version.toString()))
+    compileOnly(kotlin("reflect", project.version.toString()))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Bundle-License" to "http://www.apache.org/licenses/LICENSE-2.0.txt",
+                    "Bundle-SymbolicName" to "org.jetbrains.kotlin.jdk8-osgi-bundle",
+                    "Export-Package" to "kotlin.*;-split-package:=merge-first",
+                    "Import-Package" to "!*"
+            ))
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "CitcOss"
+            url = uri("https://maven1.citc.ru/content/repositories/citc-opensource")
+            credentials {
+                username = project.properties["mavenPublishUser.${url.host}"].toString()
+                password = project.properties["mavenPublishPassword.${url.host}"].toString()
+            }
+        }
+    }
+
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+}
